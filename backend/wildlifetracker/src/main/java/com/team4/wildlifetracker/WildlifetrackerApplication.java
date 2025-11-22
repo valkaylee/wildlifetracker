b4c9cb46701340c3fd3fa27a34fc403af0432523
Sean Harbison <spharbis@usc.edu>
2025-11-22 08:15:07 -0800

    Backend setup: added Spring Boot project and MySQL configuration

---
+++ backend/wildlifetracker/src/main/java/com/team4/wildlifetracker/WildlifetrackerApplication.java
@@ -0,0 +1,13 @@
+package com.team4.wildlifetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildlifetrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildlifetrackerApplication.class, args);
+	}
+
+}
---
+++ backend/wildlifetracker/src/test/java/com/team4/wildlifetracker/WildlifetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team4.wildlifetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildlifetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
