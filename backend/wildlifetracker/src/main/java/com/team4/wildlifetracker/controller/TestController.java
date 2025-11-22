5622379aa8a8f2c0561a47ecfd442c56f79dd583
Sean Harbison <spharbis@usc.edu>
2025-11-22 08:56:31 -0800

    Added SecurityConfig and TestController for initial backend routing

---
+++ backend/wildlifetracker/src/main/java/com/team4/wildlifetracker/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.team4.wildlifetracker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/test")
+    public String test() {
+        return "Backend is running!";
+    }
+}
