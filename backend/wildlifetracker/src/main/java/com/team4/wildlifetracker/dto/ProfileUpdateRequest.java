3a0634b0e956c6e2abc92c98df785a4f17b53252
sriyap27 <sriya.pratipati@gmail.com>
2025-12-02 11:22:56 -0800

    Added leaderboard and user profile functionality
    User profile has bio, display name, and profile picture.
    Leaderboard functionality includes profile statistics and overall leaderboard

---
+++ backend/wildlifetracker/src/main/java/com/team4/wildlifetracker/dto/ProfileUpdateRequest.java
@@ -0,0 +1,33 @@
+package com.team4.wildlifetracker.dto;
+
+public class ProfileUpdateRequest {
+    private String displayName;
+    private String bio;
+    private String profilePictureUrl;
+
+    public ProfileUpdateRequest() {}
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getProfilePictureUrl() {
+        return profilePictureUrl;
+    }
+
+    public void setProfilePictureUrl(String profilePictureUrl) {
+        this.profilePictureUrl = profilePictureUrl;
+    }
+}
